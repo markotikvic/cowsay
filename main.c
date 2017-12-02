4e4bcc6b65593ff35918a823e01d2dae85b50cbb
markotikvic <markotikvic@gmail.com>
2017-12-02 02:39:42 +0100

    minor cleanup

--- main.c
+++ main.c
@@ -2,24 +2,32 @@
 #include <string.h>
 
 /* cow without escaped characters
-     \    ^__^
-      \   (oo)\______
-          (__)\      )\/\
-              ||---w |
-              ||    ||
- */
+     \  ^__^
+      \ (oo)\______
+        (__)\      )\/\
+            ||---w |
+            ||    ||
+*/
 
 // believe it or not, this is a cow
 char cow[] ="\n\
-     \\    ^__^              \n\
-      \\   (oo)\\______      \n\
-          (__)\\      )\\/\\ \n\
-              ||---w |       \n\
-              ||    ||       \n";
+     \\  ^__^              \n\
+      \\ (oo)\\______      \n\
+        (__)\\      )\\/\\ \n\
+            ||---w |       \n\
+            ||    ||       \n";
 
 void print_usage()
 {
-    printf("Usage: cowsay \"<something>\"\n");
+    printf("Usage: cowsay \"<your text goes here>\"\n");
+}
+
+void dashed_line(int l)
+{
+    printf(" ");
+    for (int i = 0; i < l+2; i++)
+        printf("_");
+    printf(" \n");
 }
 
 int main(int argc, char **argv)
@@ -29,23 +37,11 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    // top line
-    int ll = strlen(argv[1]);
-    printf(" ");
-    for (int i = 0; i < ll + 2; i++)
-        printf("_");
-    printf(" \n\n");
-
-    // text
+    int lsize = strlen(argv[1]);
+    dashed_line(lsize);
+    printf("\n");
     printf("< %s >\n", argv[1]);
-
-    // bottom line
-    printf(" ");
-    for (int i = 0; i < ll + 2; i++)
-        printf("_");
-    printf(" \n");
-
-    // cow
+    dashed_line(lsize);
     printf("%s\n", cow);
 
     return 0;
